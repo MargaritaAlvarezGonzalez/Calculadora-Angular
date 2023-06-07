69b49cd174cfd305b5b49a7f73a0027641845e88
ws-malvagonzale\malvagonzalez <malvagonzalez@proxya.com>
2023-06-07 10:42:06 +0200

    added modularization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   titulo = 'Aplicación Calculadora';
-  operandoA : number = 0;
-  operandoB : number = 0;
-  resultado : number = 0;
+  
+  resultadoPadre : number;
 
-  sumar(): void{
-    this.resultado = this.operandoA + this.operandoB;
+  procesarResultado(resultado: number){
+  this.resultadoPadre= resultado;
   }
+  
 }
